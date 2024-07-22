e1e3225c9217b6aa3580d5bd341cd425c6a09021
PaaSam <qaugustine08@gmail.com>
2024-07-22 18:22:27 +0000

    added a login and logout functionalities

--- todo/views.py
+++ todo/views.py
@@ -1,29 +1,44 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView,CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Task
 
 # Create your views here.
-class Tasklist(ListView):
+
+class CustomLoginView(LoginView):
+    template_name = 'todo/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+
+class Tasklist(LoginRequiredMixin,ListView):
     model = Task
     context_object_name = 'tasks' #this overides the default queryset name of 'object_list' to tasks
 
-class TaskDetail(DetailView):
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'todo/task.html'
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
-class TaskUpdate(UpdateView):
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
-class TaskDelete(DeleteView):
+class TaskDelete(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
